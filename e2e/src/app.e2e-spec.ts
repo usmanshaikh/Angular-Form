82ddeb8abff019be0ece12bd9e56840e07098cd6
unknown <shaikh.usman@blackstraw.ai>
2019-09-16 20:22:36 +0530

    Angular-Form project added to gitlab

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to AnalyticsFox!');
+    expect(page.getTitleText()).toEqual('Welcome to AngularForm!');
   });
 
   afterEach(async () => {
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,8 +6,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
-  imagePath: string =
-    "https://firebasestorage.googleapis.com/v0/b/you-watch-now.appspot.com/o/analyticsfox-logo.png?alt=media&token=ce39d49a-bdaa-43f7-bdd5-01ab414826c9";
+  imagePath: string = "../assets/logo.png"
   constructor() {}
 
   ngOnInit() {}
